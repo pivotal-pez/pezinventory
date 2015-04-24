2d31dcd0e9c4193bf49c5d5ce72b5054bd136ce5
John Calabrese <jcalabrese@pivotal.io>
2015-04-24 09:29:09 -0400

    [#92251946] slight refactor
    changing how we inject the auth middleware...
    
    seems much cleaner to pass it in that
    break everything apart as i did previously

--- service/server.go
+++ service/server.go
@@ -10,7 +10,7 @@ type Server *martini.ClassicMartini
 
 //NewServer configures and returns a Server.
 //TODO: Parameterize DB
-func NewServer() (m Server, addRoutes func()) {
+func NewServer(authHandler martini.Handler) (m Server) {
 
 	m = Server(martini.Classic())
 	m.Use(render.Renderer(render.Options{
@@ -19,21 +19,19 @@ func NewServer() (m Server, addRoutes func()) {
 
 	//TODO: Database
 
-	addRoutes = func() {
-		m.Group("/v1/types", func(r martini.Router) {
-			ctrl := &TypeController{}
-			r.Get("", ctrl.listTypes)
-			r.Get("/:id", ctrl.getType)
-			r.Get("/:id/items", ctrl.listTypeItems)
-		})
+	m.Group("/v1/types", func(r martini.Router) {
+		ctrl := &TypeController{}
+		r.Get("", ctrl.listTypes)
+		r.Get("/:id", ctrl.getType)
+		r.Get("/:id/items", ctrl.listTypeItems)
+	}, authHandler)
 
-		//items routes
-		m.Group("/v1/items", func(r martini.Router) {
-			ctrl := &ItemController{}
-			r.Get("", ctrl.listItems)
-			r.Get("/:id", ctrl.getItem)
-			r.Get("/:id/history", ctrl.getItemHistory)
-		})
-	}
+	//items routes
+	m.Group("/v1/items", func(r martini.Router) {
+		ctrl := &ItemController{}
+		r.Get("", ctrl.listItems)
+		r.Get("/:id", ctrl.getItem)
+		r.Get("/:id/history", ctrl.getItemHistory)
+	}, authHandler)
 	return
 }
